14fe43ff407f46510c568c6a4554e757230724ff
Florian <f0208r@gmail.com>
2024-03-04 19:19:38 +0100

    [ADD] display items

--- script.js
+++ script.js
@@ -11,4 +11,28 @@ const fetchData = async () => {
     }
 }
 
-fetchData();
+const addItemToContainer = (item, item_container) => {
+    const itemDiv = document.createElement('div',);
+    itemDiv.classList.add('item');
+    const titleElment = document.createElement('h4');
+    const imgElement = document.createElement('img');
+    const priceElement = document.createElement('p');
+
+    titleElment.textContent = item.name;
+    imgElement.src = apiUrl + 'picture/' + item._id;
+    priceElement.textContent = item.price;
+    itemDiv.appendChild(titleElment);
+    itemDiv.appendChild(imgElement);
+    itemDiv.appendChild(priceElement);
+    item_container.appendChild(itemDiv);
+}
+
+const DisplayItems = async () => {
+    const data = await fetchData();
+    let item_container = document.querySelector('.item-container');
+    data.forEach(item => {
+        addItemToContainer(item, item_container);
+    });
+}
+
+DisplayItems();

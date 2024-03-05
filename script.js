e799b818c34b6f091bdabc1a9d9cd292580be8a1
Florian <f0208r@gmail.com>
2024-03-05 21:39:28 +0100

    [ADD] button add to cart

--- script.js
+++ script.js
@@ -11,19 +11,33 @@ const fetchData = async () => {
     }
 }
 
+// Define the cart
+let cart = JSON.parse(localStorage.getItem('cart')) || [];
+
+// Define the addToCart function
+const addToCart = (item) => {
+    cart.push(item);
+    localStorage.setItem('cart', JSON.stringify(cart));
+    console.log(cart);
+}
+
 const addItemToContainer = (item, item_container) => {
     const itemDiv = document.createElement('div',);
     itemDiv.classList.add('item');
     const titleElment = document.createElement('h4');
     const imgElement = document.createElement('img');
     const priceElement = document.createElement('p');
+    const addToCartButton = document.createElement('button');
 
     titleElment.textContent = item.name;
     imgElement.src = apiUrl + 'picture/' + item._id;
     priceElement.textContent = item.price;
+    addToCartButton.textContent = 'Add to Cart';
+    addToCartButton.addEventListener('click', () => addToCart(item));
     itemDiv.appendChild(titleElment);
     itemDiv.appendChild(imgElement);
     itemDiv.appendChild(priceElement);
+    itemDiv.appendChild(addToCartButton);
     item_container.appendChild(itemDiv);
 }
 

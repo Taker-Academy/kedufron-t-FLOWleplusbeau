411bb9a53fa9ad89d5fa67ed1c6a5eb6ca2fd690
Florian <f0208r@gmail.com>
2024-03-09 22:04:36 +0100

    [ADD] order

---
+++ JS/order.js
@@ -0,0 +1,33 @@
+const apiUrl = 'https://api.kedufront.juniortaker.com/order/';
+
+document.getElementById('checkout-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const address = document.getElementById('address').value;
+    const cart = JSON.parse(localStorage.getItem('cart')) || [];
+
+    const order = {
+        email: email,
+        name: name,
+        address: address,
+        cart: cart.map(item => ({
+            id: item.id,
+            amount: item.amount
+        }))
+    };
+
+    const data = JSON.stringify(order);
+
+    console.log('Order:', data);
+    axios.post(apiUrl, order)
+        .then(function(response) {
+            console.log('Order successful:', response);
+            localStorage.removeItem('cart');
+            window.location.href = 'confirmation.html';
+        })
+        .catch(function(error) {
+            console.error('Order failed:', error);
+        });
+});

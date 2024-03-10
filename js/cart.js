0066b57db95e9bde0583717b3b95d04886819ee0
Florian <f0208r@gmail.com>
2024-03-10 21:37:34 +0100

    [ADD] increment and decrement in cart

--- js/cart.js
+++ js/cart.js
@@ -13,18 +13,44 @@ const fetchData = async () => {
 // Define the cart
 let cart = JSON.parse(localStorage.getItem('cart')) || [];
 
+const incrementAmount = (id) => {
+    const item = cart.find(cartItem => cartItem.id === id);
+    item.amount++;
+    localStorage.setItem('cart', JSON.stringify(cart));
+    location.reload();
+}
+
+const decrementAmount = (id) => {
+    const item = cart.find(cartItem => cartItem.id === id);
+    if (item.amount > 1) {
+        item.amount--;
+    }
+    localStorage.setItem('cart', JSON.stringify(cart));
+    location.reload();
+}
+
 const addCartItemToContainer = (item, cartItem, cart_container) => {
     const itemDiv = document.createElement('div',);
     itemDiv.classList.add('cart-item');
     itemDiv.setAttribute('id', item._id);
+    let price = item.price * cartItem.amount;
+    price = price.toFixed(2);
 
     itemDiv.innerHTML = `
-        <h4>${item.name}</h4>
-        <img src="${apiUrl}picture/${item._id}" alt="${item.name}" />
-        <p>${item.price}</p>
-        <p>${cartItem.amount}</p>
-        <a href="html/product.html?id=${item._id}">Voir produit</a>
-        <button onclick="removeItem(${item._id})">Enlever du panier</button>
+        <img src="${apiUrl}picture/${item._id}" alt="${item.name}"/>
+        <div class="cart-item-info">
+            <h4>${item.name}</h4>
+            <p>${price}€</p>
+            <div class="amount-container">
+                <button onclick="decrementAmount(${item._id})">-</button>
+                <p>nombre: ${cartItem.amount}</p>
+                <button onclick="incrementAmount(${item._id})">+</button>
+            </div>
+            <div class="action-container">
+                <a href="html/product.html?id=${item._id}">Voir produit</a>
+                <button onclick="removeItem(${item._id})">Enlever du panier</button>
+            </div>
+        </div>
     `;
     cart_container.appendChild(itemDiv);
 }
@@ -45,10 +71,16 @@ const DisplayCart = async () => {
         const item = data.find(item => item._id.toString() === cartItem.id.toString());
         if (item) {
             addCartItemToContainer(item, cartItem, cart_container);
-        } else {
-            console.error(`No item found with id: ${cartItem.id}`);
         }
     });
+
+    let total = 0;
+    cart.forEach(cartItem => {
+        const item = data.find(item => item._id.toString() === cartItem.id.toString());
+        total += item.price * cartItem.amount;
+    });
+    total = total.toFixed(2);
+    document.getElementById('total').innerText = `Total: ${total}€`;
 }
 
 DisplayCart();
